dcceb009e98674a1bcf28a02a4fc214af8709ea2
Maddux4K <maddoxmaggio@gmail.com>
2025-08-28 22:26:21 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mobile-Detailing/', // 👈 IMPORTANT: must match your repo name exactly
+  base: '/Mobile-Detailing/', // 👈 MUST match your repo name exactly (case-sensitive)
 })
